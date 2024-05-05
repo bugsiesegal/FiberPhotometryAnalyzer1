Fixed the errors and added new normalization.

data.py
@@ -1,32 +1,32 @@
 import os
+from glob import glob
 
+import pandas as pd
 import torch
 from lightning.pytorch import LightningDataModule
 from torch.utils.data import DataLoader, Dataset
-import torch
-import torch.nn.functional as F
+
 from config import Config
-from glob import glob
-import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler, QuantileTransformer, \
+    PowerTransformer
 
 
 class FiberTrackingDataset(Dataset):
     """Dataset for fiber and tracking data."""
-    def __init__(self, data, window_size, use_fiber=True, use_tracking=True, normalize=True):
+
+    def __init__(self, data, window_size, use_fiber=True, use_tracking=True):
         """
         Initialize the dataset with data configurations.
         :param data: List of tuples (fiber data, tracking data)
         :param window_size: Size of the data window
         :param use_fiber: Boolean, whether to include fiber data
         :param use_tracking: Boolean, whether to include tracking data
-        :param normalize: Boolean, whether to normalize data
         """
         self.data = data
         self.window_size = window_size
         self.len_data = sum(fiber.shape[0] - window_size - 1 for fiber, _ in self.data)
         self.use_fiber = use_fiber
         self.use_tracking = use_tracking
-        self.normalize = normalize
 
     def __len__(self):
         return self.len_data
@@ -40,13 +40,10 @@ class FiberTrackingDataset(Dataset):
 
     def _get_window(self, fiber, tracking, idx):
         """Extracts a window of data from the dataset."""
+        # Get window
         window_end_idx = idx + self.window_size
         fiber_window = fiber[idx:window_end_idx].unsqueeze(1)
         tracking_window = self._get_tracking_window(tracking, fiber, idx)
-
-        if self.normalize:
-            fiber_window = self._normalize(fiber_window)
-            tracking_window = self._normalize(tracking_window)
 
         if self.use_fiber and self.use_tracking:
             return torch.hstack((fiber_window, tracking_window))
@@ -61,16 +58,12 @@ class FiberTrackingDataset(Dataset):
         subset_indices = tracking_indices[idx:idx + self.window_size].round().long().clamp(0, tracking.shape[0] - 1)
         return tracking[subset_indices]
 
-    def _normalize(self, data):
-        """Normalizes the data to the range [0, 1]."""
-        min_val, max_val = data.min(dim=0).values, data.max(dim=0).values
-        return (data - min_val) / (max_val - min_val)
-
 
 class FiberTrackingDataModule(LightningDataModule):
     """
     A data module for loading and preparing fiber tracking data using PyTorch Lightning.
     """
+
     def __init__(self, config: Config):
         """
         Initialize the data module with configuration.
@@ -85,6 +78,22 @@ class FiberTrackingDataModule(LightningDataModule):
 
         self.fiber_tracking_pairs = []
         self.data = []
+
+    def normalizer(self):
+        if self.config.normalization == 'min-max':
+            return MinMaxScaler()
+        elif self.config.normalization == 'standard':
+            return StandardScaler()
+        elif self.config.normalization == 'robust':
+            return RobustScaler()
+        elif self.config.normalization == 'max-abs':
+            return MaxAbsScaler()
+        elif self.config.normalization == 'quantile':
+            return QuantileTransformer()
+        elif self.config.normalization == 'power':
+            return PowerTransformer()
+        else:
+            raise ValueError(f"Normalization method {self.config.normalization} not supported.")
 
     def load_fiber(self, fiber_path):
         """Load and process fiber data from a CSV file."""
@@ -120,6 +129,21 @@ class FiberTrackingDataModule(LightningDataModule):
         tracking = tracking.to(torch.float32)
         return tracking
 
+    def fit_normalization(self, data):
+        """Fit normalization to data."""
+        # Get data shape
+        data_shape = data[0][0].shape
+        # Flatten data
+        flat_data = torch.cat([fiber.flatten() for fiber, _ in data])
+        # Reshape data
+        flat_data = flat_data.reshape(-1, data_shape[0])
+        # Fit normalization
+        self.normalizer().fit(flat_data)
+
+    def normalize_data(self, data):
+        """Normalize data using the fitted normalization."""
+        return [(self.normalizer().transform(fiber), tracking) for fiber, tracking in data]
+
     def prepare_data(self) -> None:
         """Prepare data by loading paths and asserting correct matches."""
         # Get fiber and tracking paths
@@ -139,6 +163,12 @@ class FiberTrackingDataModule(LightningDataModule):
             for fiber_path, tracking_path in self.fiber_tracking_pairs
         ]
 
+        # Fit normalization
+        self.fit_normalization(self.data)
+
+        # Normalize data
+        self.data = self.normalize_data(self.data)
+
     def setup(self, stage=None):
         """Split data into train, val, test sets."""
         # Split data into train, val, test
@@ -151,7 +181,8 @@ class FiberTrackingDataModule(LightningDataModule):
 
     def train_dataloader(self):
         return DataLoader(
-            FiberTrackingDataset(self.train_data, self.config.window_dim, self.config.use_fiber, self.config.use_tracking, self.config.normalize),
+            FiberTrackingDataset(self.train_data, self.config.window_dim, self.config.use_fiber,
+                                 self.config.use_tracking, self.config.normalize),
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=True
@@ -159,7 +190,8 @@ class FiberTrackingDataModule(LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(
-            FiberTrackingDataset(self.val_data, self.config.window_dim, self.config.use_fiber, self.config.use_tracking, self.config.normalize),
+            FiberTrackingDataset(self.val_data, self.config.window_dim, self.config.use_fiber, self.config.use_tracking,
+                                 self.config.normalize),
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=False
@@ -167,7 +199,8 @@ class FiberTrackingDataModule(LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(
-            FiberTrackingDataset(self.test_data, self.config.window_dim, self.config.use_fiber, self.config.use_tracking, self.config.normalize),
+            FiberTrackingDataset(self.test_data, self.config.window_dim, self.config.use_fiber,
+                                 self.config.use_tracking, self.config.normalize),
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=False

models/fft_transformer_model.py
@@ -16,6 +16,15 @@ class FFTTransformerAutoencoder_1(TransformerAutoencoder_1):
         config.input_features = config.input_features * 2
         super(FFTTransformerAutoencoder_1, self).__init__(config)
 
+        if config.activation == 'relu':
+            self.output_activation = torch.nn.ReLU()
+        elif config.activation == 'sigmoid':
+            self.output_activation = torch.nn.Sigmoid()
+        elif config.activation == 'tanh':
+            self.output_activation = torch.nn.Tanh()
+        else:
+            raise ValueError(f"Activation function {config.activation} not supported.")
+
     def forward(self, x):
         """The forward pass"""
         x = torch.fft.fft(x, dim=1)
@@ -23,5 +32,5 @@ class FFTTransformerAutoencoder_1(TransformerAutoencoder_1):
         x = self.encoder(x)
         x = self.decoder(x)
         x = torch.fft.ifft(torch.complex(x[..., :x.shape[-1] // 2], x[..., x.shape[-1] // 2:]), dim=1).real
-        x = F.sigmoid(x)
+        x = self.output_activation(x)
         return x

models/transformer_model_v1.py
@@ -51,12 +51,21 @@ class TransformerEncoder(BaseEncoder):
                 nhead=config.nhead,
                 dim_feedforward=config.dim_feedforward,
                 dropout=config.dropout,
-                activation=config.activation,
+                activation='relu',
                 batch_first=True
             ),
             num_layers=config.num_layers
         )
         self.encoder_compression = nn.Linear(config.window_dim * config.d_model, config.latent_dim)
+
+        if config.activation == 'relu':
+            self.output_activation = nn.ReLU()
+        elif config.activation == 'sigmoid':
+            self.output_activation = nn.Sigmoid()
+        elif config.activation == 'tanh':
+            self.output_activation = nn.Tanh()
+        else:
+            raise ValueError(f"Activation function {config.activation} not supported.")
 
     def forward(self, x):
         """Forward pass through the transformer encoder. Returns the latent representation."""
@@ -64,7 +73,7 @@ class TransformerEncoder(BaseEncoder):
         x = self.positional_encoding(x.transpose(0, 1)).transpose(0, 1)
         x = self.transformer_encoder(x).transpose(1, 2).reshape(x.shape[0], -1)
         x = self.encoder_compression(x)
-        x = F.sigmoid(x)
+        x = self.output_activation(x)
         return x
 
 
@@ -84,12 +93,21 @@ class TransformerDecoder(BaseDecoder):
                 nhead=config.nhead,
                 dim_feedforward=config.dim_feedforward,
                 dropout=config.dropout,
-                activation=config.activation,
+                activation='relu',
                 batch_first=True
             ),
             num_layers=config.num_layers
         )
         self.output_layer = nn.Linear(config.d_model, config.input_features)
+
+        if config.activation == 'relu':
+            self.output_activation = nn.ReLU()
+        elif config.activation == 'sigmoid':
+            self.output_activation = nn.Sigmoid()
+        elif config.activation == 'tanh':
+            self.output_activation = nn.Tanh()
+        else:
+            raise ValueError(f"Activation function {config.activation} not supported.")
 
     def forward(self, x):
         """Forward pass through the transformer decoder. Returns the reconstructed input."""

models/transformer_model_v2.py
@@ -29,6 +29,15 @@ class TransformerEncoder(BaseEncoder):
             num_layers=config.num_layers
         )
 
+        if config.activation == 'relu':
+            self.output_activation = nn.ReLU()
+        elif config.activation == 'sigmoid':
+            self.output_activation = nn.Sigmoid()
+        elif config.activation == 'tanh':
+            self.output_activation = nn.Tanh()
+        else:
+            raise ValueError(f"Activation function {config.activation} not supported.")
+
     def forward(self, x):
         """Forward pass through the transformer encoder. Returns the latent representation."""
         x = self.input_layer(x)
@@ -37,7 +46,7 @@ class TransformerEncoder(BaseEncoder):
         x = x.reshape(x.shape[0], -1)
         # Get the last segment of the output
         x = x[:, -self.config.latent_dim:]
-        x = F.sigmoid(x)
+        x = self.output_activation(x)
         return x
 
 
@@ -63,12 +72,23 @@ class TransformerDecoder(BaseDecoder):
             num_layers=config.num_layers
         )
         self.output_layer = nn.Linear(config.d_model, config.input_features)
+        self.padding = nn.ConstantPad1d((0, config.window_dim * config.d_model - config.latent_dim), 0)
+
+        if config.activation == 'relu':
+            self.output_activation = nn.ReLU()
+        elif config.activation == 'sigmoid':
+            self.output_activation = nn.Sigmoid()
+        elif config.activation == 'tanh':
+            self.output_activation = nn.Tanh()
+        else:
+            raise ValueError(f"Activation function {config.activation} not supported.")
 
     def forward(self, x):
         """Forward pass through the transformer decoder. Returns the reconstructed input."""
-        x = F.pad(x, (0, self.config.window_dim * self.config.d_model - self.config.latent_dim)).reshape(x.shape[0], -1, self.config.d_model)
+        x = self.padding(x).reshape(x.shape[0], -1, self.config.d_model)
         x = self.transformer_decoder(x)
         x = self.output_layer(x)
+        x = self.output_activation(x)
         return x
 
 